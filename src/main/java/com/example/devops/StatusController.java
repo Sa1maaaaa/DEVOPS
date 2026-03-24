a04186e140ece950533c66d18968f00bc02f709b
Faris <faris@example.com>
2026-03-24 14:34:12 +0100

    Initial DevOps Stack for Prof. Farrenkopf

---
+++ src/main/java/com/example/devops/StatusController.java
@@ -0,0 +1,13 @@
+package com.example.devops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/")
+    public String status() {
+        return "DevOps Projekt - Gruppe Faris - Status: Online";
+    }
+}
