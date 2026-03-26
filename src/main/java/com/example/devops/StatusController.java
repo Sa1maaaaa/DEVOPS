ad5b61e48a6aa4ea3e394cb99a8c91f3d30b1eab
Faris <faris@example.com>
2026-03-26 23:49:21 +0100

    Update: Finaler Text fuer die Praesentation

--- src/main/java/com/example/devops/StatusController.java
+++ src/main/java/com/example/devops/StatusController.java
@@ -8,6 +8,6 @@ public class StatusController {
 
     @GetMapping("/")
     public String status() {
-        return "Hallo Welt 123455- Status: Online";
+        return "Hallo Welt 1 - Status: Online";
     }
 }
