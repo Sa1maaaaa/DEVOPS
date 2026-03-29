877fa287ac7a870cd97e03268ad3c8e6c0462438
Sa1maaaaa <168195937+Sa1maaaaa@users.noreply.github.com>
2026-03-29 12:23:52 +0000

    Video-Demo: Live Text-Update

--- src/main/java/com/example/devops/StatusController.java
+++ src/main/java/com/example/devops/StatusController.java
@@ -8,6 +8,6 @@ public class StatusController {
 
     @GetMapping("/")
     public String status() {
-        return "Hallo Welt - Status: Online";
+        return "Devops-Video-Demonstration - Status: Online";
     }
 }
